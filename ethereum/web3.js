23025ca5c5b367ef3136292fa5b707e9aab6bd02
darshjain <darshj786@gmail.com>
2021-07-19 23:21:59 +0530

    Adding Semantic UI react

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -4,7 +4,8 @@ let web3;
 
 if(typeof window !== 'undefined' && window.web3 !== 'undefined'){
   web3 = new Web3(window.web3.currentProvider);
-}else{
+}
+else{
   const provider = new Web3.providers.HttpProvider(
     'https://rinkeby.infura.io/v3/3a4fe6fd68534f858f1f393b0a8f322d'
   );
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react'
 import factory from '../ethereum/factory'
 
 class CampaignIndex extends Component{
-    async componentDidMount(){
-        const campaign = await factory.methods.getDeployedCampaigns().call()
-        console.log(campaign)
-        
+    static async getInitialProps() {
+        const campaigns = await factory.methods.getDeployedCampaigns().call()
+        return { campaigns }
     }
     render(){
-        return <div>Campaign Index!</div>
+        return <div>{ this.props.campaigns[0] }</div>
     }
 }
 

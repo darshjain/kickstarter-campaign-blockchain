2dce1bc564e9b469d46d9895601468e3829f338c
darshjain <darshj786@gmail.com>
2022-03-19 20:59:10 +0530

    States

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Input } from 'semantic-ui-react'
 import Layout from '../../components/Layout'
 
 class CampaignNew extends Component {
+  state = {
+    minimumContribution: '',
+    nameCampaign: '',
+  }
   render() {
     return (
       <Layout>
@@ -10,11 +14,23 @@ class CampaignNew extends Component {
         <Form>
           <Form.Field>
             <label>Name Of The Startup</label>
-            <input />
+            <Input
+              value={this.state.nameCampaign}
+              onChange={(event) =>
+                this.setState({ nameCampaign: event.target.value })
+              }
+            />
           </Form.Field>
           <Form.Field>
             <label>Minimum Contribution</label>
-            <input />
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={(event) =>
+                this.setState({ minimumContribution: event.target.value })
+              }
+            />
           </Form.Field>
           <Button primary>Create!</Button>
         </Form>
